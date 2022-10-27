58cb11749357f8e48e127211ae72e1790fe35c34
Nyberg988 <carl.nyberg@hotmail.com>
2022-10-27 14:27:17 +0000

    deployment

--- sailor_sam/settings.py
+++ sailor_sam/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
