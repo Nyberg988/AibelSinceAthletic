f2936f5efa0e5195f1bf8a5c19d265a9c669884c
Nyberg988 <carl.nyberg@hotmail.com>
2022-11-22 09:27:55 +0000

    fixed the 404 error

--- sailor_sam/urls.py
+++ sailor_sam/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'sailor_sam.views.handler404'
